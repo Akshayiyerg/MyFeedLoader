55d7340d0ae82da969888ae9c526e545568065a0
Akshay Ganesh <akshayiyerg@gmail.com>
2023-06-17 17:16:39 -0400

    Guarantee correct URLs and methods on get request.

--- MyFeedLoaderTests/Feed API/URLSessionHTTPClientTests.swift
+++ MyFeedLoaderTests/Feed API/URLSessionHTTPClientTests.swift
@@ -27,6 +27,24 @@ class URLSessionHTTPClient {
 
 class URLSessionHTTPClientTests: XCTestCase {
     
+    func test_getFromURL_performGETRequestWithURL() {
+        URLProtocolStub.startInterceptiongRequests()
+        let url = URL(string: "https://any-url.com")!
+        
+        let exp = expectation(description: "wait for the request")
+        
+        URLProtocolStub.observeRequests { request in
+            XCTAssertEqual(request.url, url)
+            XCTAssertEqual(request.httpMethod, "GET")
+            exp.fulfill()
+        }
+        
+        URLSessionHTTPClient().get(from: url) { _ in }
+        
+        wait(for: [exp], timeout: 1.0)
+        URLProtocolStub.stopInterceptiongRequests()
+    }
+    
     func test_getFromURL_failsFromRequestError() {
         
         URLProtocolStub.startInterceptiongRequests()
@@ -61,11 +79,16 @@ class URLSessionHTTPClientTests: XCTestCase {
     private class URLProtocolStub: URLProtocol {
         
         private static var stub: Stub?
+        private static var requestObserver: ((URLRequest) -> Void)?
         
         private struct Stub {
             let data: Data?
             let response: URLResponse?
             let error: Error?
+        }
+        
+        static func observeRequests(observer: @escaping (URLRequest) -> Void) {
+            requestObserver = observer
         }
         
         static func startInterceptiongRequests() {
@@ -75,6 +98,7 @@ class URLSessionHTTPClientTests: XCTestCase {
         static func stopInterceptiongRequests() {
             URLProtocol.unregisterClass(URLProtocolStub.self)
             stub = nil
+            requestObserver = nil
         }
         
         static func stub(data: Data?, response: URLResponse?, error: Error?) {
@@ -82,6 +106,7 @@ class URLSessionHTTPClientTests: XCTestCase {
         }
         
         override class func canInit(with request: URLRequest) -> Bool {
+            requestObserver?(request)
             return true
         }
         
